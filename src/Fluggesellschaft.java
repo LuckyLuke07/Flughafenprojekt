f136c51f346c15a2b875f1986e285917c387b9c6
lmoser <lukas.moser@sf-ag.com>
2018-12-03 00:02:27 +0100

    class Fluggesellschaft added

---
+++ src/Fluggesellschaft.java
@@ -0,0 +1,28 @@
+
+public class Fluggesellschaft {
+	
+	private String name; 
+	private String ort; 
+	private String strasse; 
+	
+	public Fluggesellschaft() {}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getOrt() {
+		return ort;
+	}
+
+	public String getStrasse() {
+		return strasse;
+	}
+	
+	public String getKuerzel() {
+		return ""; 
+	}
+	
+	
+
+}
